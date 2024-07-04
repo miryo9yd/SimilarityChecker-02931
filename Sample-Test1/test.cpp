e7d58381bf0f0ca030b0abc33df8b8f1cfadf56c
[youngdae.seo] <miryo9yd@naver.com>
2024-07-04 09:15:38 +0900

    Length Logic Commit

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,21 @@
 #include "pch.h"
 #include "../SimilarityCheck/similarity.cpp"
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+
+TEST(TestCaseName, SameLengthCheck) {
+	Similarity sim;
+	int ret = sim.checklength("AAA","BBB");
+	EXPECT_EQ(60, ret);
+}
+
+TEST(TestCaseName, DoubleLengthCheck) {
+	Similarity sim;
+	int ret = sim.checklength("AAA", "BBBBBBB");
+	EXPECT_EQ(0, ret);
+}
+
+TEST(TestCaseName, PartialPoint) {
+	Similarity sim;
+	int ret = sim.checklength("AAA", "BBBBB");
+	EXPECT_EQ(20, ret);
 }
--- SimilarityCheck/similarity.cpp
+++ SimilarityCheck/similarity.cpp
@@ -1,3 +1,45 @@
-class Similarity {
+#include <string>
+#include <iostream>
+#include<algorithm>
+using namespace std;
 
+class Similarity {
+public :
+	int checklength(const string& input1, const string& input2) {
+
+		int LongLength = getLongStrLenth(input1, input2);
+		int ShortLength = getShortStrLenth(input1, input2);
+
+
+		if (isSameStrLength(LongLength, ShortLength)) return  MAX_POINT;
+		if (isDoubleLength(LongLength, ShortLength)) return  0;
+		return getStrDiffPoint(LongLength, ShortLength);
+
+
+	}
+	int getStrDiffPoint(int LongLength, int ShortLength)
+	{
+		return MAX_POINT - MAX_POINT * (LongLength - ShortLength) / ShortLength;
+	}
+	bool isDoubleLength(int LongLength, int ShortLength)
+	{
+		return LongLength >= ShortLength * 2;
+	}
+	bool isSameStrLength(int LongLength, int ShortLength)
+	{
+		return LongLength == ShortLength;
+	}
+	int getLongStrLenth(const string& input1, const string& input2) {
+
+		if (input1 > input2) return input1.length();
+		return input2.length();
+	}
+
+	int getShortStrLenth(const string& input1, const string& input2) {
+
+		if (input1 < input2) return input1.length();
+		return input2.length();
+	}
+private : 
+	const int MAX_POINT = 60; 
 };
